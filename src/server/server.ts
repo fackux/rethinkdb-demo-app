3b8a655518a87a16aa3421331d50972f66dbdbd9
fackux <flopez@navent.com>
2021-11-06 20:19:29 -0300

    cre conecion de prueba server-client

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,15 +1,40 @@
 import express from 'express'
 import next from 'next'
+import socketIO from 'socket.io'
 
 const port = 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const options = {
+    cors: {
+        origin: '*',
+    }
+}
+
+const io = new socketIO.Server(options)
+
+io.on('connect', (client) => {
+    console.log('IO:Client Subscribe -> Messages', client.id)
+
+    client.on('disconnect', () => {
+        console.log('Unsubscribe IO:Client -> Messages', client.id)
+    })
+
+    setInterval(() => {
+        console.log('emit changes to', client.id)
+        client.emit('CHANGES', { randomNumber: Math.random() * 100 })
+    }, 5000)
+})
+
+io.listen(3001)
+
+
 app.prepare().then(() => {
     const server = express()
 
-    server.all('*', (req:any, res:any) => {
+    server.all('*', (req: any, res: any) => {
         return handle(req, res)
     })
 
